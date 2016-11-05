7efaa49bda23f70aa884b025b72eb47f70900c35
hua.ai <13348867336@163.com>
2016-11-05 11:12:03 +0800

    init version

--- src/main/java/ai/Application.java
+++ src/main/java/ai/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AiSpringCamelKafkaApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AiSpringCamelKafkaApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
